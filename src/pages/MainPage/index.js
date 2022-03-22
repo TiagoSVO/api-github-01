e479c5d0830af99174b1c09d6a1eba90b7c58bf2
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-03-22 10:35:55 -0300

    [FEATURE] Inserted the action to submit on main page

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 import ImageLogo from '../../assets/imgs/github-logo.svg';
 import { MdSearch } from 'react-icons/md';
@@ -7,16 +8,35 @@ import { Container, Wrapper, Title, Logo, Form, Input, Button } from './styles'
 
 const MainPage = () => {
   const [login, setLogin] = useState('');
+
+  const navigate = useNavigate();
+
+  const redirectToGitPage = (login) => {
+    if(login !== '') {
+      console.log(`${login}`)
+    }
+  }
+
+  const onClickButtonSearch = (event, login) => {
+    event.preventDefault()
+    redirectToGitPage(login)
+  }
+
+  const onSubmit = (event, login) => {
+    event.preventDefault()
+    redirectToGitPage(login)
+  }
+
   return(
     <Container>
       <Wrapper>
         <Logo src={ImageLogo}/>
         <Title>API Github</Title>
-        <Form onSubmit={() => alert(`The username is ${login}`)}>
+        <Form onSubmit={(event) => onSubmit(event, login)}>
           <Input placeholder='username' 
                  value={login} 
                  onChange={(e) => setLogin(e.target.value)}/>
-          <Button type='button' onClick={() => alert(`The username is ${login}`)}>
+          <Button type='button' onClick={(event) => onClickButtonSearch(event, login)}>
             <MdSearch size={25}/>
           </Button>
         </Form>
