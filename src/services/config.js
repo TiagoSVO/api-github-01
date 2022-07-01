22e301ca19afd079799639ffb2add8f2fa7c66c8
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-07-01 15:45:18 -0300

    [FEATURE] Created tools to help render colors and languages of the repositories

---
+++ src/services/config.js
@@ -0,0 +1,18 @@
+export const languagesColors = {
+    typescript: '#00569d',
+    php: '#5861a1',
+    go: "#72aed8",
+    vue: "#2ecc71",
+    python: "#27ae60",
+    css: "#4e97d1",
+    c: "#2980b9",
+    'c++': "#3498db",
+    'c#': "#8e44ad",
+    scss: "#9b59b6",
+    javascript: "#f1c40f",
+    html: "#e67e22",
+    ruby: "#c0392b",
+    rails: "#c0392b",
+    shell: "#ecf0f1",
+}
+
