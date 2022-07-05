d47b855bbef55a10291a41b476619282b9ba5365
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-07-05 16:48:53 -0300

    [UPDATE] Changed from BrowserRouter to HashRouter for deploy

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage'
 import GitPage from './pages/GitPage'
 
 export default function Router() {
   return (
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/git-page/:login" element={<GitPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
